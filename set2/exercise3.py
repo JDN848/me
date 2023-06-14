51a16ea4f0b283a1aa15b844a115fd93a3f60140
Jayden Tran <z5360741@ad.unsw.edu.au>
2023-06-14 15:26:08 +1000

    git.suggestSmartCommit

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -258,14 +258,14 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    pyramid = []
+    """pyramid = []
     for i in range(5):
         pyramid.append(pyramid_row)
         pyramid_row = []
         for j in range(9):
             pyramid_row.append("*")
         pyramid.append(pyramid_row)
-    return pyramid
+    return pyramid"""
 
 
 def little_printer(some_kind_of_list, exercise_name):
